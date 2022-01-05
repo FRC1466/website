bf9596ed7666f2ac28737c41e2a2e965c0aa02eb
2023popn <2023popn@webbstudent.org>
2022-01-05 10:31:42 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
-document.getElementsByTagName('body')[0].style.cssText += 'display: grid; grid-template-rows: auto 400px 200px 200px 200px auto; grid-template-columns: 40% 20% 40%;';
+var currentFileName = location.pathname.split("/").slice(-1);
+console.log(currentFileName);
+
+var body = document.getElementsByTagName('body')[0];
+
+switch(currentFileName){
+    case "index.html":
+        body.style.cssText += 'display: grid; grid-template-rows: auto 400px 200px 200px 200px auto; grid-template-columns: 40% 20% 40%;';
+        break;
+}
