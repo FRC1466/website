1b760d7b49bce5238fd21babe769e349fe9e2228
2023popn <2023popn@webbstudent.org>
2022-01-10 09:57:04 -0500

    remove grid

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ console.log(currentFileName);
 var body = document.getElementsByTagName('body')[0];
 
 if(currentFileName == "index.html"){
-    body.style.cssText += 'display: grid; grid-template-rows: auto 400px 200px 200px 200px auto; grid-template-columns: 40% 20% 40%;';
+    //body.style.cssText += 'display: grid; grid-template-rows: auto 400px 200px 200px 200px auto; grid-template-columns: 40% 20% 40%;';
 }
 
 
