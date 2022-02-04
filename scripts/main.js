e280da38e6da8419a1bfdc936dd61271ab4d40ab
nt <89554543+7910f6ba7ee4@users.noreply.github.com>
2022-02-04 17:08:39 -0500

    improved index.html

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,12 @@ var currentFileName = location.pathname.split("/").slice(-1);
 console.log(currentFileName);
 
 var body = document.getElementsByTagName('body')[0];
+var footer = body.getElementsByTagName('footer')[0];
 
-if(currentFileName == "index.html"){
-    body.style.cssText += 'display: grid; grid-template-rows: auto 400px 200px 200px 200px auto; grid-template-columns: 40% 20% 40%;';
+if(currentFileName == "index.html") {
+    body.style.cssText += 'display: grid';
+    footer.style.cssText += 'display:inline-grid; grid-row:3;';
+    
 }
 
 
