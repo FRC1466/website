e553f13da3c8a5530d9c957090e3e7534c38afdb
nt <89554543+7910f6ba7ee4@users.noreply.github.com>
2022-02-14 17:19:55 -0500

    changed header appearance and added changed first pages

--- scripts/header-main.js
+++ scripts/header-main.js
@@ -1,15 +1,31 @@
 let header = document.createElement('header');
 
+let indexWrap = document.createElement('div')
+indexWrap.setAttribute('class', 'title-container');
 let indexDiv = document.createElement('div')
 indexDiv.setAttribute('id', 'title');
+indexWrap.appendChild(indexDiv);
 let indexImg = document.createElement('img');
 indexImg.setAttribute('src', 'media/webblogo.png')
 indexDiv.appendChild(indexImg);
 let indexLink = document.createElement('a')
 indexLink.appendChild(document.createTextNode('Webb Robotics'));
 indexLink.setAttribute('href', 'index.html');
+
+let headerWrap = document.createElement('div')
+headerWrap.setAttribute('class', 'header-container');
+headerWrap.appendChild(indexWrap);
+
+
+let indexTeam = document.createElement('div')
+indexTeam.setAttribute('class', 'team-header');
+let indexTeamNum = document.createElement('p')
+indexTeam.appendChild(indexTeamNum);
+indexTeamNum.appendChild(document.createTextNode('1466'))
 indexDiv.appendChild(indexLink);
-header.appendChild(indexDiv);
+headerWrap.appendChild(indexWrap);
+headerWrap.appendChild(indexTeam);
+header.appendChild(headerWrap);
 
 pages_list = [null, 'first.html', 'calendar.html', 'photo-gallery.html', null];
 text_list = ['About', 'FIRST Robotics', 'Upcoming Events', 'Photo Gallery', 'Sponsors'];
