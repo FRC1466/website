b6962a1810e8dba8063f19df9cfdad57c5405887
webbrobotics <webbrobotics@gmail.com>
2022-03-30 17:29:41 -0400

    reword sponsors

--- src/pages/team/Sponsors.js
+++ src/pages/team/Sponsors.js
@@ -4,9 +4,9 @@ function Sponsors(){
     return(
         <div className="Sponsors">
             <p>
-                Work in progress, will have some info here soon. <br/> <br/> For inquiries, regardless of team, 
+                This page is a work in progress, we will have more info here soon. <br/> <br/> For inquiries, regardless of team, 
                 please email webbrobotics@gmail.com. We are currently in the process of revamping our team communications system, 
-                so this method could be different in the future.
+                so this method could be different in the future. 
             </p>
 
         </div>
