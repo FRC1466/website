c6c169958625aa35c38dc7290d10449cf54cdf34
webbrobotics <webbrobotics@gmail.com>
2022-03-30 16:49:38 -0400

    small sponsor desc

---
+++ src/pages/team/Sponsors.js
@@ -0,0 +1,15 @@
+import '../../styles.css';
+
+function Sponsors(){
+    return(
+        <div className="Sponsors">
+            <p>
+                Work in progress, will have some info here soon. <br/> <br/> For inquiries, regardless of team, 
+                please email webbrobotics@gmail.com. We are currently in the process of revamping our team communications system, 
+                so this method could be different in the future.
+            </p>
+
+        </div>
+    );
+}
+export default Sponsors;
