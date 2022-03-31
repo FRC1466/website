f8d08bcded91065585766578dcc5ceb444c686b6
Dmitri K <69704069+DtPeach3707@users.noreply.github.com>
2022-03-30 23:43:05 -0400

    Outreach page started

--- src/pages/FRC/Outreach.js
+++ src/pages/FRC/Outreach.js
@@ -3,12 +3,50 @@ import '../../styles.css';
 function FRC_Outreach(){
     return(
         <div className="FRC">
-            <strong>
+            <strong className='article-title'>
                 Our FRC Team's Outreach
             </strong>
+            <ul className='training-table'>
+            <li>
             <p>
-                Work in progress, will have some info here soon
+                Many of our FRC team members mentor our FTC and FLL programs, using their
+                experience to aid the younger members in their robotics journeys.
             </p>
+            </li>
+            <li>
+            <p>
+                Our team members participate in community service at 2nd Harvest and other local nonprofit organizations.
+            </p>
+            </li>
+            </ul>
+            <ul className='training-table'>
+            <li>
+            <img className='outreach-img' src="https://drive.google.com/uc?export=view&id=1JcGOfQ43jpCAXbZaiQgsNCgKK7nfqPjc" />
+            </li>
+            <li>
+            <img className='outreach-img' src="https://drive.google.com/uc?export=view&id=1k9NRWErxZuMaQ4yRSUJTjA1GviyVvKsg" />
+            </li>
+            </ul>
+            <ul className='training-table'>
+            <li>
+            <p>
+                During quarantine in 2020, team members 3D printed mask "ear savers" from home to distribute to people in need.
+            </p>
+            </li>
+            <li>
+                <p>
+                Team members volunteer at local STEM summer camps, helping kids foster an interest in science and engineering.
+                </p>
+            </li>
+            </ul>
+            <ul className='training-table'>
+            <li>
+            <img className='outreach-img' src="https://drive.google.com/uc?export=view&id=1AJk3l2_po8B_BCNoIF9DFH3rHXr-4quv" />
+            </li>
+            <li>
+                <img className='outreach-img' src="https://drive.google.com/file/duc?export=view&id=1laTr2jhz4Y9Y_2MDvLfANtWDkx_3oHgH" />
+            </li>
+            </ul>
         </div>
     );
 }
